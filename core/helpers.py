49fb3d854e2ef818f8d68f9b909e03c8c5a5018d
ehsan <1374ea@gmail.com>
2020-11-03 16:22:23 +0330

    make core functionality better

--- core/helpers.py
+++ core/helpers.py
@@ -1,6 +1,7 @@
 import os
 import smtplib
 import ssl
+import sys
 from email.mime.base import MIMEBase
 
 from jinja2 import Environment, select_autoescape, FileSystemLoader
@@ -46,3 +47,36 @@ def jinja2_env():
     )
     env.globals['asset'] = asset
     return env
+
+
+def clean_command_args(sys_argv):
+    try:
+        if sys_argv[0] == 'python':
+            del sys_argv[0]
+
+        if os.path.isfile(sys_argv[0]):
+            del sys_argv[0]
+
+        return sys_argv
+    except KeyError:
+        print("Invalid command")
+        sys.exit()
+
+
+def generate_help_text(help_data):
+    for command_name, command_data in help_data.items():
+        print(command_name + ':')
+
+        if command_data.get('description'):
+            print(f'\t{command_data["description"]}\n')
+
+        if command_data.get('args'):
+            for arg_name, arg_description in command_data['args'].items():
+                print(f'\t- {arg_name}: {arg_description}')
+        else:
+            print('\t(no need argument)')
+
+        print()
+
+
+
--- runner.py
+++ runner.py
@@ -3,13 +3,36 @@ import subprocess
 import sys
 
 from core.config import ACTIONS_DIR, BASE_DIR
+from core.helpers import clean_command_args
+
+
+def check_path(path, message):
+    if not os.path.exists(path):
+        print(message)
+        sys.exit()
+
 
 if __name__ == '__main__':
-    action_name = sys.argv[1]
+    command_args = clean_command_args(sys.argv)
+
+    action_name = command_args.pop(0)
     action_dir = os.path.join(ACTIONS_DIR, action_name)
-    action_runner = os.path.join(ACTIONS_DIR, action_name, 'run.py')
+    check_path(action_dir, 'Invalid action')
 
     os.environ['PYTHONPATH'] = BASE_DIR
     os.environ['RUNNING_ACTION_DIR'] = action_dir
 
-    subprocess.run(['python', action_runner])
+    if 'help' in command_args:
+        del command_args[0]
+        runner = os.path.join(action_dir, 'help.py')
+        check_path(runner, 'Action not contain help command')
+    else:
+        runner = os.path.join(action_dir, 'run.py')
+        check_path(runner, 'Invalid command')
+
+    try:
+        subprocess.run(['python', runner, *command_args])
+    except KeyboardInterrupt:
+        print('Process interrupted by user')
+    except:
+        print('Something went wrong')
