37d7273c373e883c7f342169d864a9e9134d0813
ehsan <1374ea@gmail.com>
2020-11-03 16:20:47 +0330

    move python specific commands from old cheatsheet project to this

---
+++ actions/python/help.py
@@ -0,0 +1,25 @@
+from core.helpers import generate_help_text
+
+help_data = {
+    'install': {
+        'description': 'install python on machine',
+        'args': {
+            'version': '3.5, ~~3~~, ~~2~~'
+        }
+    },
+    'virtualenv': {
+        'description': 'install and create virtualenv',
+        'args': {
+            'path': 'absolute path for creating virtualenv',
+            'version': 'version of python, ex. python3',
+            'name': 'virtualenv name without any "env" prefix or suffix',
+            'settings': 'relative path of project settings in python mode'
+        }
+    },
+    'locale': {
+        'description': 'fix "Python locale error: unsupported locale setting" error'
+    },
+}
+
+if __name__ == '__main__':
+    generate_help_text(help_data)
