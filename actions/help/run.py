b01d6315b7e5018d98a04650686a535b61043ac2
ehsan <1374ea@gmail.com>
2020-11-03 14:34:22 +0330

    improve help action response

--- actions/help/run.py
+++ actions/help/run.py
@@ -6,8 +6,12 @@ if __name__ == '__main__':
     print('Available Actions:')
 
     _, dir_names, _ = next(walk(ACTIONS_DIR))
+    actions = []
     for dir_name in dir_names:
-        print('\t', dir_name)
+        actions.append(dir_name)
+
+    actions.sort()
+    print('\n'.join(f'\t{a}' for a in actions))
 
     print('Usage:')
     print('\tpython runner.py <action_name>')
