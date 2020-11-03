94b334935bd4c760d89ecafc5fbd6a00beb8e12f
ehsan <1374ea@gmail.com>
2020-11-03 16:46:26 +0330

    move deploy specific commands from old cheatsheet project to this

---
+++ actions/deploy/help.py
@@ -0,0 +1,25 @@
+from core.helpers import generate_help_text
+
+help_data = {
+    'scaffolding': {
+        'description': """
+        create base for project in production server, somethings like below
+        
+        <project>
+        ├── app
+        │   └── <root of project content>
+        ├── etc
+        │   └── nginx
+        │       └── sites-available
+        └── var
+            └── log
+                └── <project>
+        """,
+        'args': {
+            'project': 'project name'
+        }
+    }
+}
+
+if __name__ == '__main__':
+    generate_help_text(help_data)
